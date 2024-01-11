ec7cf9af36536b9e446da033df8df6a776b7ecbc
Anton <itonyemail@gmail.com>
2024-01-10 22:44:16 -0800

    typo

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -10,26 +10,26 @@ public class BallSpawner : MonoBehaviour{
         for(int i = 0; i < numberOfBalls; i++){
             SpawnBall();
         }
+    }
 
-        void SpawnBall(){
-            int attempts = 0;
-            bool placed = false;
+    void SpawnBall(){
+        int attempts = 0;
+        bool placed = false;
 
-            while(!placed && attempts < maxSpawnAttemptsPerBall){
-                Vector3 position = RandomPosition();
-                if(!Physics.CheckSphere(position, spawnRadius)){
-                    Instantiate(ballPrefab, position, Quaternion.identity);
-                    placed = true;
-                }
-                attempts++;
+        while(!placed && attempts < maxSpawnAttemptsPerBall){
+            Vector3 position = RandomPosition();
+            if(!Physics.CheckSphere(position, spawnRadius)){
+                Instantiate(ballPrefab, position, Quaternion.identity);
+                placed = true;
             }
+            attempts++;
         }
+    }
 
-        Vector3 RandomPosition(){
-            float x = Random.Range(-5f, 5f);
-            float y = Random.Range(-5f, 5f);
-            float z = Random.Range(-5f, 5f);
-            return new Vector3(x, y, z);
-        }
+    Vector3 RandomPosition(){
+        float x = Random.Range(-5f, 5f);
+        float y = Random.Range(-5f, 5f);
+        float z = Random.Range(-5f, 5f);
+        return new Vector3(x, y, z);
     }
 }
