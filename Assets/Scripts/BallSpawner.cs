67eadbb807ea98cf11f663b354d41a505cd75361
Anton <itonyemail@gmail.com>
2024-01-11 15:55:53 -0800

    settings update

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class BallSpawner : MonoBehaviour{
     public GameObject ballPrefab;
     public int numberOfBalls = 10;
-    public Vector3 startPoint = new Vector3(0, 0, 0);
-    public Vector3 endPoint = new Vector3(9, 0, 0);
+    public Vector3 startPoint = new Vector3(0, 1, 0);
+    public Vector3 endPoint = new Vector3(9, 1, 0);
 
     void Start(){
         for(int i = 0; i < numberOfBalls; i++){
