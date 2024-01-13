cc02edc2da6117af8071efacce057cb72ee3af08
Anton <itonyemail@gmail.com>
2024-01-12 17:09:14 -0800

    moving code around

--- Assets/Scripts/BallPositionUpdater.cs
+++ Assets/Scripts/BallPositionUpdater.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Mathematics;
 
-public class BallPositionUpdater : MonoBehaviour{
+[BurstCompile]
+public struct UpdateBallPositionsJob : IJobParallelFor{
+    public NativeArray<float3> positions;
+    public float deltaTime;
+    public float3 gravity;
+
+    public void Execute(int index){
+        positions[index] += gravity * deltaTime;
+    }
+}
+
+public class MyJobRunner : MonoBehaviour{
     void Start(){
         int numBalls = 1000;
         var ballPositions = new NativeArray<float3>(numBalls, Allocator.TempJob);
 
-        UpdateBallPositionsJob positionJob = new UpdateBallPositionsJob{
-            ballPositions = ballPositions,
+        UpdateBallPositionsJob job = new UpdateBallPositionsJob
+        {
+            positions = ballPositions,
             deltaTime = Time.deltaTime,
             gravity = new float3(0, -9.81f, 0)
         };
 
-        JobHandle positionJobHandle = positionJob.Schedule(numBalls, 64);
-        positionJobHandle.Complete();
+        JobHandle jobHandle = job.Schedule(numBalls, 64);
+        jobHandle.Complete();
 
-        //Apply updated positions to balls
+        // Apply updated positions to balls
+        // ...
 
         ballPositions.Dispose();
     }
